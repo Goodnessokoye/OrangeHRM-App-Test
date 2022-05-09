fd83f94a4424eaf4674e403d27aebdc7e2517660
Goodness <goodnessokoye10@gmail.com>
2022-05-09 18:17:52 +0100

    added recorded video by cypress

--- cypress/integration/Simple Automation Projects/OrangeHRM.spec.js
+++ cypress/integration/Simple Automation Projects/OrangeHRM.spec.js
@@ -1,24 +1,51 @@
 ///<reference types="cypress"/>
 
 describe("OrangeHRM", () => {
-  it("Valid credential", () => {
+  it("Launch the App", () => {
     cy.visit("https://opensource-demo.orangehrmlive.com/", {
       retryOnNetworkFailure: true,
     });
-    cy.get("#divLogo > img").should("be.visible");
+  });
+  it("Login with valid email & valid password", () => {
     cy.get("#logInPanelHeading").should("have.text", "LOGIN Panel");
     cy.get("#txtUsername").type("Admin");
     cy.get("#txtPassword").type("admin123");
     cy.get("#btnLogin").should("be.visible");
-    cy.get("#btnLogin").click();
+    cy.get("#btnLogin").click(), { retryOnNetworkFailure: true };
+  });
+
+  it("LogOut", () => {
     cy.get("#welcome").click();
     cy.get("#welcome-menu > :nth-child(1) > :nth-child(3) > a").click();
     cy.get("#divLogo > img").should("be.visible");
+    cy.wait(500);
   });
+
+  it("Login with valid email & invalid password", () => {
+    cy.get("#txtUsername").type("Admin");
+    cy.get("#txtPassword").type("goodness123");
+    cy.get("#btnLogin").should("be.visible");
+    cy.get("#btnLogin").click();
+    cy.wait(1000);
+  });
+
   it("invalid email and valid password", () => {
     cy.get("#txtUsername").type("goodness");
     cy.get("#txtPassword").type("admin123");
     cy.get("#btnLogin").click();
     cy.get("#spanMessage").should("be.visible");
+    cy.wait(1000)
+  });
+
+  it("Login with no credentials", () => {
+    cy.get("#txtUsername").type(" ");
+    cy.get("#txtPassword").type(" ");
+    cy.get("#btnLogin").click();
+    cy.get("#spanMessage").should("be.visible");
+    cy.wait(1000);
+  });
+
+  it("That login was unsuccessful with the error “Invalid credentials” returned", () => {
+    cy.get("#spanMessage").should("have.text", "Csrf token validation failed");
   });
 });
--- cypress/integration/Simple Automation Projects/todoApp.spec.js
+++ cypress/integration/Simple Automation Projects/todoApp.spec.js
@@ -17,30 +17,27 @@ describe("Todo List App", () => {
     cy.get(".destroy").invoke("show");
     cy.wait(5000);
     cy.get(".destroy").click();
-    cy.wait(5000);
-    cy.get(".toggle").click();
+    cy.wait(2000);
+  });
+  it("Add 5 different items", () => {
+    cy.get(".new-todo")
+      .type("Hit the gym {enter}")
+      .type("Hot bath{enter}")
+      .type("Eat{enter}")
+      .type("Go to work{enter}")
+      .type("Hit the driving school{enter}");
+  });
+  it("Delete 3 items from the list", () => {
+    cy.get(":nth-child(1) > .view > .toggle").click();
+    cy.get(":nth-child(3) > .view > .toggle").click();
+    cy.get(":nth-child(5) > .view > .toggle").click();
     cy.wait(5000);
     cy.get(".clear-completed").click();
+    cy.wait(2000);
+    cy.get(".clear-completed").should("not.exist");
+    cy.wait(3000);
+    cy.get(":nth-child(3) > a").click();
+    cy.wait(3000);
+    cy.get(":nth-child(1) > a").click();
   });
-    it("Add 5 different items", () => {
-      cy.get(".new-todo")
-        .type("Hit the gym {enter}")
-        .type("Hot bath{enter}")
-        .type("Eat{enter}")
-        .type("Go to work{enter}")
-        .type("Hit the driving school{enter}");
-    });
-    it("Delete 3 items from the list", () => {
-      cy.get(":nth-child(1) > .view > .toggle").click();
-      cy.get(":nth-child(3) > .view > .toggle").click();
-      cy.get(":nth-child(5) > .view > .toggle").click();
-      cy.wait(5000);
-      cy.get(".clear-completed").click();
-      cy.wait(2000);
-      cy.get(".clear-completed").should("not.exist");
-      cy.wait(3000);
-      cy.get(":nth-child(3) > a").click();
-      cy.wait(3000);
-      cy.get(":nth-child(1) > a").click();
-    });
 });
